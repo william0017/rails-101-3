981284602361f89989529fe7b4c8993840ad35e2
william0017E <wangwei0017@gmail.com>
2016-10-08 16:47:42 +0800

    implement my post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,15 +18,16 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(group_params)
-    @group.user = current_user
-    if @group.save
-      current_user.join!(@group)
-      redirect_to groups_path
-    else
-      render :new
+      @group = Group.new(group_params)
+      @group.user = current_user
+      if @group.save
+        current_user.join!(@group)
+        redirect_to groups_path
+      else
+        render :new
+      end
+
     end
-  end
 
   def update
     if @group.update(group_params)
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
   end
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
+
 end
